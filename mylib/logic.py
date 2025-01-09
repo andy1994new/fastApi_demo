44d6f25b3f069ffaeff3bcfeb3e1b6fc0f10511e
Andy G <andyg@Andys-MacBook-Air.local>
2025-01-09 06:13:53 +0000

    change cli

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,6 +1,9 @@
 import wikipedia
 
-
 def wiki(name="War Goddess", length=1):
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def wiki_search(name="War"):
+    result = wikipedia.search(name)
+    return result
